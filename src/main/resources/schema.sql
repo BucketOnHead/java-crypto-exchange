8b298d797121090bdfb69d76f5dcb6973cd5b6d1
IvanMarakanov <marakanovivan@gmail.com>
2023-02-26 12:07:47 +0300

    update: prohibit-null values for the key

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    user_id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    username   VARCHAR(100) UNIQUE NOT NULL,
-    email      VARCHAR(100) UNIQUE NOT NULL,
-    secret_key VARCHAR(34) UNIQUE  NOT NULL
+    user_id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    username   VARCHAR(100) UNIQUE                             NOT NULL,
+    email      VARCHAR(100) UNIQUE                             NOT NULL,
+    secret_key VARCHAR(34) UNIQUE                              NOT NULL
 );
